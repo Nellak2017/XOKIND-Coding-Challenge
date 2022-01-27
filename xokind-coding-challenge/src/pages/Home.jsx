af35551d5617c3c13c8b1050ca488a96b641e216
Connor Keenum <Nellakprime@gmail.com>
2022-01-27 03:35:55 -0600

    Made my App CRUD capable for the first time! I am almost done fulfilling all the requirements for the project.

--- xokind-coding-challenge/src/pages/Home.jsx
+++ xokind-coding-challenge/src/pages/Home.jsx
@@ -1,5 +1,6 @@
-import React from 'react';
-import app, { auth } from "../firebase.js"; // Used to Talk to Firebase API
+import React, { useEffect, useState } from 'react';
+import { auth, colRef } from "../firebase.js"; // Used to Talk to Firebase API
+import { getDocs, addDoc } from "firebase/firestore"; // Used to talk to Firestore API
 import Nav from '../components/Nav/Nav';
 import SideNav from '../components/SideNav/SideNav';
 import Carosel from '../components/Carosel/Carosel';
@@ -12,6 +13,38 @@ import { useHistory } from 'react-router-dom';
 
 const Home = () => {
     const RouteHistory = useHistory();
+
+    const [places, setPlaces] = useState([]);
+    const [isAdding, setIsAdding] = useState(false);
+    const [isEditing, setIsEditing] = useState(false);
+    const [isDeleting, setIsDeleting] = useState(false);
+
+    // Upon entering Home page, grab all the Places to be used only Once
+    useEffect(() => {
+        const docs = getDocs(colRef).then((snapshot) => {
+            setPlaces([]);
+            snapshot.docs.forEach((doc) => {
+                setPlaces(old => [...old, { ...doc.data(), id: doc.id }]);
+            })
+        }).catch(err => {
+            alert(err);
+        });
+    }, [isAdding, isEditing, isDeleting]);
+
+    const handleAdd = () => {
+        setIsAdding(!isAdding);
+        addDoc(colRef, { 
+            name: "Default Name",
+            imageUrl: "https://picsum.photos/200",
+            rating: 3,
+            price: "$$"
+        })
+            .then()
+            .catch((err) => alert(err))
+    }
+    const handleEdit = () => { setIsEditing(!isEditing) }
+    const handleDelete = () => { setIsDeleting(!isDeleting) }
+
     return (
         <>
             <IconContext.Provider value={{ color: '#fff' }}>
@@ -22,22 +55,22 @@ const Home = () => {
                             <button className="nav-button">Sort by: Rating (placeholder)</button>
                             <button
                                 className="nav-button"
-                                onClick={() => {RouteHistory.push("/login"); auth.signOut();}}
+                                onClick={() => { RouteHistory.push("/login"); auth.signOut(); }}
                             >
                                 Sign Out
                             </button>
                         </span>
                     </Nav>
                     <SideNav>
-                        <IoMdAdd size={32} className="icon" />
-                        <AiFillEdit size={32} className="icon" />
-                        <MdDeleteForever size={32} className="icon" />
+                        <IoMdAdd size={32} className="icon" onClick={handleAdd} />
+                        <AiFillEdit size={32} className="icon" onClick={handleEdit} />
+                        <MdDeleteForever size={32} className="icon" onClick={handleDelete} />
                     </SideNav>
                     <CarouselContainer>
                         <h2 style={{ fontSize: '2rem', color: 'black', margin: '2rem 0 2rem 3.5rem' }}>
                             List of Places
                         </h2>
-                        <Carosel />
+                        <Carosel data={places} setData={setPlaces} isAdding={isAdding} setIsAdding={setIsAdding} isEditing={isEditing} setIsEditing={setIsEditing} isDeleting={isDeleting} setIsDeleting={setIsDeleting} />
                     </CarouselContainer>
                 </>
             </IconContext.Provider>
